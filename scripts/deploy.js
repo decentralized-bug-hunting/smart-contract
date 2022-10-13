5e31958e2861fbf198243bff3930657e2b892544
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-10-13 12:35:06 +0545

    contract and deply script initialized

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+
+  const Token = await hre.ethers.getContractFactory("DeBounty");
+  const debounty = await Token.deploy();
+  await debounty.deployed();
+
+  console.log("DeBounty address: ", debounty.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
